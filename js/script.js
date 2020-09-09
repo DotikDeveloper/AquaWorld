2d7e8e5c7bc8bda312b69ae7a79e86b7275fdde2
Dotik <36793535+DotikDeveloper@users.noreply.github.com>
2020-09-09 16:51:18 +0400

    add scroll to link

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    // scroll -----------------------
+
+    const anchors = document.querySelectorAll('a.scroll-to');
+
+    for (const anchor of anchors) {
+        anchor.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href');
+
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
+
+    // scroll --------------------------------------------------------
+
+});
