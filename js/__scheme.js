ea0c3a298bae836ae6121b1073998e619710fc75
Dotik <36793535+DotikDeveloper@users.noreply.github.com>
2020-10-10 16:29:39 +0400

    add class and button

--- js/__scheme.js
+++ js/__scheme.js
@@ -1,3 +1,31 @@
+const calcSubmit = document.querySelector('#calcSubmit'),
+turbidity = document.querySelector('#turbidity').value,
+chromaticity = document.querySelector('#chromaticity').value,
+smell = document.querySelector('#smell').value,
+rigidity = document.querySelector('#rigidity').value,
+iron = document.querySelector('#iron').value,
+manganese = document.querySelector('#manganese').value,
+permanganate = document.querySelector('#permanganate').value,
+hydrogen = document.querySelector('#hydrogen').value,
+ph = document.querySelector('#ph').value,
+mineralization = document.querySelector('#mineralization').value,
+ammonium = document.querySelector('#ammonium').value,
+nitrates = document.querySelector('#nitrates').value,
+residents = document.querySelector('#residents').value,
+points = document.querySelector('#points').value,
+peak = document.querySelector('#peak').value,
+name = document.querySelector('#name').value,
+email = document.querySelector('#email').value,
+tel = document.querySelector('#tel').value;
+
+
+calcSubmit.addEventListener('click', () => {
+console.log('вызов формы');
+
+
+
+});
+
 let scheme = (
     turbidity,
     chromaticity,
